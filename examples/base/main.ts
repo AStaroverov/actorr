a4b226cf77055967a937d1ef2f047e2dc2f18c1b
Alexandr <hellbeast92@gmail.com>
2023-02-03 00:17:07 +0100

    mapper for connectors + unification code

--- examples/base/main.ts
+++ examples/base/main.ts
@@ -2,13 +2,15 @@ import {createActorSum} from "../common/actors/sum/actor";
 import {createActorMinMax} from "../common/actors/minmax/actor";
 import {createActorMain} from "../common/actors/main/actor";
 import {connectActorToActor} from "../../main";
+import {SUM_ACTION_TYPE} from "../common/actors/sum/defs";
+import {MINMAX_ACTION_TYPE} from "../common/actors/minmax/defs";
 
 const actorSum = createActorSum();
 const actorMinMax = createActorMinMax();
 const actorMain = createActorMain();
 
-connectActorToActor(actorMain, actorSum);
-connectActorToActor(actorMain, actorMinMax);
+connectActorToActor({ source: actorMain, map: (e) => e.type === SUM_ACTION_TYPE ? e : undefined }, actorSum);
+connectActorToActor({ source: actorMain, map: (e) => e.type === MINMAX_ACTION_TYPE ? e : undefined }, actorMinMax);
 
 actorSum.launch();
 actorMinMax.launch();
