28cfe1f33c925ae0aef7050190427d9d66760ff3
Alexandr <hellbeast92@gmail.com>
2023-02-03 18:55:43 +0100

    remove log

--- examples/common/actors/sum/actor.ts
+++ examples/common/actors/sum/actor.ts
@@ -4,7 +4,6 @@ import {SUM_ACTION_TYPE, SUM_RESULT_TYPE, TSumActionEnvelope, TSumResultEnvelope
 
 export function createActorSum() {
     return createActor<TSumActionEnvelope|TSumResultEnvelope>('SUM', (envelope, { dispatch }) => {
-        console.log('>>', envelope)
         if (envelope.type === SUM_ACTION_TYPE) {
             dispatch(createEnvelope(SUM_RESULT_TYPE, envelope.payload.reduce((acc, v) => acc + v, 0)))
         }
